a1d7a2a3c69c3b4dac6a7bb7cd57d36292a78d67
kdurh <93518445+kdurh@users.noreply.github.com>
2022-02-16 00:15:07 +0900

    업로드 에러 500

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,30 +1,22 @@
-var file = document.querySelector('MyFile');
-var result;
-
-var reader = new FileReader(file);
-reader.onload = function() {
-    result = reader.result;
+function getBase64(file){
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        const base64String = reader.result
+            .replace("data:", "")
+            .replace(/^.+,/, "");;
+        console.log(base64String);
+        return base64String;
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
 }
 
-// 실패할 경우 에러 출력하기
-reader.onerror = function (error) {
-    console.log('Error');
-};
 
-var settings = {
-    "url": "http://3.38.89.188:20115/api/image/upload",
-    "method": "POST",
-    "dataType" : "json",
-    "timeout": 0,
-    "headers": {
-        "Content-Type": "application/json"
-    },
-    "data": JSON.stringify({
-        "extension": "png",
-        "base64": result
-    }),
-};
-var settings = {
+
+
+var settings2 = {
     "url": "http://3.38.89.188:20115/api/image/",
     "method": "GET",
     "dataType" : "json",
@@ -39,29 +31,52 @@ var main ={
         });
     },
     upload : function (){
-        $.ajax(settings).done(function (response) {
+
+        var file = document.querySelector('#MyFile').files[0];
+        var result=getBase64(file); // prints the base64 string
+
+        var settings1 = {
+            "url": "http://3.38.89.188:20115/api/image/upload",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({
+                "extension": "png",
+                "base64": result
+            }),
+        };
+
+        $.ajax(settings1).done(function (response) {
             alert('사진이 업로드 되었습니다.');
+            console.log(response);
             window.location.href='/';
             // console.log(response);
         }).fail(function (error){
             alert('실패');
+            //console.log(error);
         });
     },
 
 };
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
+
+
+
+$.ajax(settings2).done(function (response) {
+    //console.log(response);
     array1 = Object.values(response);
     array2 = Object.values(array1[2]);
-    console.log(array2);
+   // console.log(array2);
     // const main = document.querySelector("#main");
     array2.forEach(function (item){
          //var tag = "<article class='thumb'><a class='image' style='background-image: url('"+item+"'); cursor: pointer; outline: 0px;'><img src='"+item+"'alt='' style='display: none;'></a><h2>&nbsp</h2></article>"
 
         // var tag = "<article className= 'thumb'><a class='image' style='background-image: url(&quot;"+item+"&quot;); cursor: pointer; outline: 0px;'><img src='"+item+"'alt='' style='display: none;'></a><h2>&nbsp</h2></article>";
 
-          var tag = "<article className= 'thumb'><a href='"+ item +"' className='image'><img src='"+item+"' alt=''/></a><h2>&nbsp</h2></article>";
+          var tag ="<article class='thumb'><a href='"+item+"' class='image'><img src='"+item+"' alt=''/></a><h2>&nbsp</h2></article>";
+        // var tag = "<article><a className=\"image\" style=\"cursor: pointer; outline: 0px;\"><img src=\""+item+"\" alt=\"\" style=\"display: none;\"></a><h2>&nbsp</h2></article>"
 
           // const article = document.createElement("article");
         // article.className="thumb";
@@ -81,10 +96,10 @@ $.ajax(settings).done(function (response) {
         // $('#main').appendChild(article);
 
         $('#main').append(tag);
-    });
 
+    });
 }).fail(function (error){
-    console.log('로드실패');
+   // console.log('로드실패');
 });
 main.init();
 
