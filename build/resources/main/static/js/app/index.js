93f307c6697db25e036824160d68c2f504ef6f21
kdurh <93518445+kdurh@users.noreply.github.com>
2022-02-16 11:40:55 +0900

    json,비동기 해결

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,30 +1,3 @@
-function getBase64(file){
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-        const base64String = reader.result
-            .replace("data:", "")
-            .replace(/^.+,/, "");
-        return base64String;
-    };
-    reader.onerror = function (error) {
-        console.log('Error: ', error);
-    };
-}
-function filetype(file){
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-        end=reader.result.search(";");
-        const filetype = reader.result.substring(11,end)
-        return filetype;
-
-    };
-    reader.onerror = function (error) {
-        console.log('Error: ', error);
-    };
-}
-
 var settings2 = {
     "url": "http://3.38.89.188:20115/api/image/",
     "method": "GET",
@@ -32,6 +5,21 @@ var settings2 = {
     "timeout": 0,
 };
 
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result.replace("data:", "")
+        .replace(/^.+,/, ""));
+    reader.onerror = error => reject(error);
+});
+
+const getfiletypeA = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result.replace("data:image/", "")
+        .replace(/;.*/, ""));
+    reader.onerror = error => reject(error);
+});
 var main ={
     init : function () {
         var _this =this;
@@ -39,10 +27,12 @@ var main ={
             _this.upload();
         });
     },
-    upload : function (){
-
+    upload : async function (){
         var file = document.querySelector('#MyFile').files[0];
-        var result=getBase64(file); // prints the base64 string
+
+        base64String =await toBase64(file);
+        filetype = await getfiletypeA(file);
+
         var settings1 = {
             "url": "http://3.38.89.188:20115/api/image/upload",
             "method": "POST",
@@ -51,19 +41,15 @@ var main ={
                 "Content-Type": "application/json"
             },
             "data": JSON.stringify({
-                "extension": filetype(file),
-                "base64": result
+                "extension": filetype,
+                "base64": base64String
             }),
         };
-
         $.ajax(settings1).done(function (response) {
             alert('사진이 업로드 되었습니다.');
-            console.log(response);
             window.location.href='/';
-            // console.log(response);
         }).fail(function (error){
             alert('실패');
-            //console.log(error);
         });
     },
 
@@ -73,11 +59,8 @@ var main ={
 
 
 $.ajax(settings2).done(function (response) {
-    //console.log(response);
     array1 = Object.values(response);
     array2 = Object.values(array1[2]);
-   // console.log(array2);
-    // const main = document.querySelector("#main");
     array2.forEach(function (item){
           var tag ="<article class='thumb'><a href='"+item+"' class='image'><img src='"+item+"' alt=''/></a><h2>&nbsp</h2></article>";
 
@@ -85,7 +68,7 @@ $.ajax(settings2).done(function (response) {
 
     });
 }).fail(function (error){
-   // console.log('로드실패');
+    console.log('로드실패');
 });
 main.init();
 
